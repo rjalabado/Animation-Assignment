59eb6454f263e0f4c06099864d8e665f44ce74e6
rjalabado <72759692+rjalabado@users.noreply.github.com>
2021-01-23 00:06:38 -0800

    Finished Version

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 var ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./sprites/patrick.png")
+ASSET_MANAGER.queueDownload("./sprites/patrick.png");
+ASSET_MANAGER.queueDownload("./sprites/patrick meme.jpg");
 
 ASSET_MANAGER.downloadAll(function () {
 	var gameEngine = new GameEngine();
 	var patrick = new Patrick(gameEngine);
+	var patrickMeme = new PatrickMeme(gameEngine);
 
 	var canvas = document.getElementById('gameWorld');
 	var ctx = canvas.getContext('2d');
 
 	gameEngine.init(ctx);
 	gameEngine.addEntity(patrick);
+	gameEngine.addEntity(patrickMeme);
 	gameEngine.start();
 });
--- patrick.js
+++ patrick.js
@@ -1,16 +1,38 @@
 class Patrick {
     constructor(game) {
         Object.assign(this, { game });
-        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/patrick.png")
-        this.animation = new Animator(this.spritesheet)
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/patrick.png");
+        this.animation = new Animator(this.spritesheet, 19, 28, 65, 92, 10, .30, 34, false, true);
+
+        // set up animator, doesnt work because of constructor
     };
 
     draw(ctx) {
-        ctx.drawImage(this.spritesheet, 0, 0)
-        //this.animation.drawFrame(this.game.clockTick, ctx, 0, 0)
+        //ctx.drawImage(this.spritesheet, 0, 0)
+        this.animation.drawFrame(this.game.clockTick, ctx, 10, 10, 3);
+         this.animation.drawFrame(this.game.clockTick, ctx, 10, 450, 3);
+        this.animation.drawFrame(this.game.clockTick, ctx, 800, 10, 3);
+        this.animation.drawFrame(this.game.clockTick, ctx, 800, 450, 3);
     }
 
     update() {
-        
+    }
+}
+
+class PatrickMeme {
+    constructor(game) {
+        Object.assign(this, { game });
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/patrick meme.jpg");
+        this.animation = new Animator(this.spritesheet, 0, 0, 498, 614, 1, 1, 34, false, true);
+
+        // set up animator, doesnt work because of constructor
+    };
+
+    draw(ctx) {
+        //ctx.drawImage(this.spritesheet, 0, 0)
+        this.animation.drawFrame(this.game.clockTick, ctx, 263, 58, 1);
+    }
+
+    update() {
     }
 }
